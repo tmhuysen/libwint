fa0ffc374dd8aba5dc8a077a9ec19179eb3d34a5
Laurent Lemmens <laurent.lemmens@ugent.be>
2017-08-16 14:19:15 +0200

    Added some more documentation. Changed to STO-3G as this is just an example.

--- main.cpp
+++ main.cpp
@@ -12,8 +12,8 @@
  */
 void compute_1body_integrals(const libint2::Operator &opertype, const libint2::BasisSet &obs, const std::vector<libint2::Atom> &atoms) {
 
-    const auto nbf = obs.nbf();     // nbf: number of basis functions
-    const auto nsh = obs.size();    // nsh: number of shells
+    const auto nbf = obs.nbf();     // nbf: number of basis functions in the obs
+    const auto nsh = obs.size();    // nsh: number of shells in the obs
 
     libint2::Engine engine (opertype, obs.max_nprim(), static_cast<int>(obs.max_l()));
 
@@ -22,20 +22,21 @@ void compute_1body_integrals(const libint2::Operator &opertype, const libint2::B
         engine.set_params(make_point_charges(atoms));
     }
 
-    const auto shell2bf = obs.shell2bf();
+    const auto shell2bf = obs.shell2bf();  // maps shell index to bf index
 
-    const auto& buffer = engine.results();      // vector that holds pointers to the shell sets
-    // the values will change after every compute() call
+    const auto& buffer = engine.results();  // vector that holds pointers to computed shell sets
+                                            // the values will change after every compute() call
 
+    // One-body integrals are between two basis functions
+    //  However, LibInt calculates integrals between shell sets, we will loop over the shells in the obs
     for(auto s1=0; s1!=nsh; ++s1) {
         for(auto s2=0; s2!=nsh; ++s2) {
 
-            // std::cout << "compute shell set {" << s1 << "," << s2 << "} ... ";
             engine.compute(obs[s1], obs[s2]);
-            // std::cout << "done" << std::endl;
-            auto ints_shellset = buffer[0];  // location of the computed integrals
+            auto ints_shellset = buffer[0];     // buffer is a vector that holds pointers to computed shell sets
+                                                // if the zeroth element is nullptr, then the whole shell set has been exhausted
             if (ints_shellset == nullptr)
-                continue;  // nullptr returned if the entire shell-set was screened out
+                continue;
 
             auto bf1 = shell2bf[s1];  // first basis function in first shell
             auto n1 = obs[s1].size(); // number of basis functions in first shell
@@ -46,7 +47,8 @@ void compute_1body_integrals(const libint2::Operator &opertype, const libint2::B
             // this iterates over integrals in this order
             for(auto f1=0; f1!=n1; ++f1) {
                 for(auto f2=0; f2!=n2; ++f2) {
-                    std::cout << "  " << bf1+f1 << " " << bf2+f2 << " " << ints_shellset[f1*n2+f2] << std::endl;
+                    auto computed_integral = ints_shellset[f1*n2 + f2];
+                    std::cout << "  " << bf1+f1 << " " << bf2+f2 << " " << computed_integral << std::endl;
                 }
             }
         }
@@ -74,16 +76,16 @@ int main() {
 
 
     // 1. MOLECULE & BASIS SET SPECIFICATION
-    const auto xyzfilename = "/Users/laurentlemmens/Documents/Xcode/LibInt_CTF/LibInt_CTF/h2o.xyz";
+    const auto xyzfilename = "/Users/laurentlemmens/Software/LibInt_to_eigen3/h2o.xyz";
     std::ifstream input_file(xyzfilename);
     auto atoms = libint2::read_dotxyz(input_file);
-    libint2::BasisSet obs ("cc-pVDZ", atoms);
+    libint2::BasisSet obs ("STO-3G", atoms);  // obs: orbital basis set
 
 
 
 
     // LEARNING AREA
-    //print_shell_sizes(obs);
+    print_shell_sizes(obs);
 
     compute_1body_integrals(libint2::Operator::overlap, obs, atoms);
 
