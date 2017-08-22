1218d7efc94012c9abd33079da3b3047e1248c9c
Laurent Lemmens <laurent.lemmens@ugent.be>
2017-08-22 15:09:29 +0200

    Everything compiles correctly now ft. Boost.test and cmake.

---
+++ tests/one_two_test.cpp
@@ -0,0 +1,10 @@
+#define BOOST_TEST_MODULE "One- and two-electron integrals"
+
+#include <boost/test/unit_test.hpp>
+#include <boost/test/included/unit_test.hpp>  // include this to get main(), otherwise clang++ will complain
+#include <libint2.hpp>
+
+BOOST_AUTO_TEST_CASE( my_test )
+{
+    
+}
