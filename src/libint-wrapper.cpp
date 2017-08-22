5cbaf5f3fa3ac13b7c4ec15343888b2e65ae05d3
Laurent Lemmens <laurent.lemmens@ugent.be>
2017-08-22 12:45:22 +0200

    New cmake version 3.8+. Now produces a library libint-wrapper.

--- src/libint-wrapper.cpp
+++ src/libint-wrapper.cpp
@@ -5,7 +5,7 @@
 #include <libint2.hpp>
 #include <Eigen/Dense>      // <Eigen/Eigen> includes <Eigen/Dense> and <Eigen/Sparse>, so we might as well just include <Eigen/Dense> since we won't be using <Eigen/Sparse> in this code
 #include <unsupported/Eigen/CXX11/Tensor>
-#include "libint-wrapper.hpp"
+#include "../include/libint-wrapper.hpp"
 
 /**
  * Given an operator type, an orbital basis and atoms, calculates the one-body integrals (associated to that operator type)
