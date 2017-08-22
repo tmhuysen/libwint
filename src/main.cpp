da058d88237d6bd012dc4faa5a223a85690ccd55
Laurent Lemmens <laurent.lemmens@ugent.be>
2017-08-22 11:18:20 +0200

    Doing a bunch of stuff related to cmake and unit testing.

--- src/main.cpp
+++ src/main.cpp
@@ -19,16 +19,17 @@ int main() {
     libint2::BasisSet obs ("STO-3G", atoms);    // obs: orbital basis set
                                                 // a libint2::BasisSet is a decorated std::vector<libint2::Shell>
 
+    std::cout << obs.size() << std::endl;
+    std::cout << obs.nbf() << std::endl;
 
     // 2. CALCULATE ONE- AND TWO BODY INTEGRALS
-    // auto S = compute_1body_integrals(libint2::Operator::overlap, obs, atoms);
+    auto S = compute_1body_integrals(libint2::Operator::overlap, obs, atoms);
     // auto T = compute_1body_integrals(libint2::Operator::kinetic, obs, atoms);
     // auto V = compute_1body_integrals(libint2::Operator::nuclear, obs, atoms);
 
     //std::cout << S << std::endl << T << std::endl << V << std::endl;
 
-    auto tei = compute_2body_integrals(obs, atoms);
-    std::cout << tei << std::endl;
+    // auto tei = compute_2body_integrals(obs, atoms);
 
     // Finalize libint2
     libint2::finalize();
