df852bcdf60d8c3b97eb0be179651120aa58a4f3
Laurent Lemmens <laurent.lemmens@ugent.be>
2017-09-18 12:34:18 +0200

    Now use an include wrapper header.

---
+++ include/libint-eigen.hpp
@@ -0,0 +1,12 @@
+#ifndef LIBINT_EIGEN_LIBINT_EIGEN_HPP
+#define LIBINT_EIGEN_LIBINT_EIGEN_HPP
+
+
+// This header file acts as a wrapper for all includes
+
+#include "Basis.hpp"
+#include "integrals.hpp"
+#include "Molecule.hpp"
+
+
+#endif //LIBINT_EIGEN_LIBINT_EIGEN_HPP
