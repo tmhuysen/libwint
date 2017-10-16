b96a764d8a9d4496c5be7869e540d5dd22c4722a
Laurent Lemmens <laurent.lemmens@ugent.be>
2017-10-16 13:48:13 +0200

    Added include file.

---
+++ include/libint-wrapper.hpp
@@ -0,0 +1,11 @@
+#ifndef LIBINT_WRAPPER_LIBINT_WRAPPER_HPP_HPP
+#define LIBINT_WRAPPER_LIBINT_WRAPPER_HPP_HPP
+
+
+// This file acts as a collective include header
+#include "Basis.hpp"
+#include "Molecule.hpp"
+
+
+
+#endif // LIBINT_WRAPPER_LIBINT_WRAPPER_HPP_HPP
