93f0e30aa9a471f28fa7460d554a04d9039e2351
Laurent Lemmens <laurent.lemmens@ugent.be>
2017-10-19 15:00:25 +0200

    Added Config and ConfigVersion.

--- cmake/InstallLibrary.cmake
+++ cmake/InstallLibrary.cmake
@@ -1,25 +1,35 @@
 # In this CMake file, we will provide the necessary commands to install the library
 
-# Installation of the target library and its associated header files
-#   The library (liblibint-wrapper.a) is installed in ${INSTALLATION_PREFIX}/lib
-#   The header files are installed in ${INSTALLATION_PREFIX}/include/libint-wrapper
 
-# 1. Install the library
-#   The target of this project is a library called libint-wrapper. We'll also have to export this target, to be able to use it in other CMake-based projects.
-#   To specify that this target should also be exported, we add the EXPORT option. This is used in conjuction with the install(EXPORT) command below
-#   The ARCHIVE option specifies that we're working with a static library
+# The target of this project is a library called libint-wrapper (${PROJECT_NAME}).
+# To specify that this target should also be exported, we add the EXPORT option. This is used in conjuction with the install(EXPORT) command below
 install(TARGETS ${LIBRARY_NAME}
         EXPORT ${LIBRARY_NAME} ${EXPORT_TYPE}
-        DESTINATION ${INSTALLATION_PREFIX}/lib)
-
-# 2. Install the header files
-install(DIRECTORY ${PROJECT_INCLUDE_FOLDER} DESTINATION ${INSTALLATION_PREFIX}/include/libint-wrapper)
-
-# 3. Export the target library: this creates a file libint-wrapper.cmake, to be loaded by an outside project
-install(EXPORT ${LIBRARY_NAME} DESTINATION ${INSTALLATION_PREFIX}/include/libint-wrapper)
-
-# This is the input file (.in) for the real version.hpp, and will be parsed by CMake to contain real version numbers
+        DESTINATION ${LIBRARY_INSTALL_DIR})
 
 
+# Parse the version.hpp.in (input - .in) file (this replaces all @VAR@ by their CMake value)
+configure_file(${PROJECT_INCLUDE_FOLDER}/version.hpp.in ${PROJECT_INCLUDE_FOLDER}/version.hpp @ONLY)
 
 
+# Install the header files
+install(DIRECTORY ${PROJECT_INCLUDE_FOLDER} DESTINATION ${INCLUDE_INSTALL_DIR})
+
+
+# Export the target library into a ${PROJECT_NAME}Targets.cmake file, to be able to use it in other CMake-based projects.
+install(EXPORT ${LIBRARY_NAME} DESTINATION ${CMAKE_INSTALL_DIR} FILE ${PROJECT_NAME}Targets.cmake)
+
+
+# Parse ${PROJECT_NAME}Config.cmake.in
+configure_file(${CMAKE_SOURCE_DIR}/cmake/Config.cmake.in ${CMAKE_SOURCE_DIR}/cmake/${PROJECT_NAME}Config.cmake @ONLY)
+
+
+# Parse ${PROJECT_NAME}ConfigVersion.cmake.in
+configure_file(${CMAKE_SOURCE_DIR}/cmake/ConfigVersion.cmake.in ${CMAKE_SOURCE_DIR}/cmake/${PROJECT_NAME}ConfigVersion.cmake @ONLY)
+
+
+# Install Config.cmake and ConfigVersion.cmake
+install(FILES
+            ${CMAKE_SOURCE_DIR}/cmake/${PROJECT_NAME}Config.cmake
+            ${CMAKE_SOURCE_DIR}/cmake/${PROJECT_NAME}ConfigVersion.cmake
+        DESTINATION ${CMAKE_INSTALL_DIR})
--- cmake/libint-wrapper-config.cmake
+++
@@ -1,5 +0,0 @@
-# Configuration file for libint-wrapper
-
-set(libint-wrapper_INCLUDE_DIRS ${INSTALLATION_PREFIX}/include/libint-wrapper)
-
-
