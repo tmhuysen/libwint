524118cedf41e41b8170a8de0d9e35e511614c3f
Laurent Lemmens <laurent.lemmens@ugent.be>
2017-10-19 13:27:35 +0200

    Refactored tests/CMakeLists

---
+++ cmake/ConfigureTest.cmake
@@ -0,0 +1,17 @@
+# In this CMake file, we will include header files and link to libraries for a given test source
+
+# ... add the boost headers (for testing) ...
+target_include_directories(${TEST_NAME} PRIVATE ${Boost_INCLUDE_DIRS})
+
+# ... add this project's headers ...
+target_include_directories(${TEST_NAME} PRIVATE ${PROJECT_INCLUDE_FOLDER})
+
+# ... add the Eigen3 headers ...
+target_link_libraries(${TEST_NAME} PUBLIC Eigen3::Eigen)
+
+# ... add the libint2 headers ...
+target_include_directories(${TEST_NAME} PRIVATE ${LIBINT_PREFIX}/include)
+target_include_directories(${TEST_NAME} PRIVATE ${LIBINT_PREFIX}/include/libint2)
+
+# ... link to this project's library ...
+target_link_libraries(${TEST_NAME} PRIVATE ${LIBRARY_NAME})
