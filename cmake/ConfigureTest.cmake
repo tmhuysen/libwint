e063897aeda49eb3bc71cd94c7ad2662469c3404
Laurent Lemmens <laurent.lemmens@ugent.be>
2017-11-25 04:59:10 +0100

    Fixed dependency issues: now libint2 'propagates' into subsequent projects.

--- cmake/ConfigureTest.cmake
+++ cmake/ConfigureTest.cmake
@@ -1,7 +1,7 @@
 # In this CMake file, we will include header files and link to libraries for a given test source
 
 # ... add the boost headers (for testing) ...
-target_include_directories(${TEST_NAME} PRIVATE ${Boost_INCLUDE_DIRS})
+target_include_directories(${TEST_NAME} PUBLIC ${Boost_INCLUDE_DIRS})
 
 # ... add this project's headers ...
 target_include_directories(${TEST_NAME} PRIVATE ${PROJECT_INCLUDE_FOLDER})
@@ -10,7 +10,7 @@ target_include_directories(${TEST_NAME} PRIVATE ${PROJECT_INCLUDE_FOLDER})
 target_link_libraries(${TEST_NAME} PUBLIC Eigen3::Eigen)
 
 # ... add the libint2 headers ...
-target_include_directories(${TEST_NAME} PRIVATE ${libint2_INCLUDE_DIRS})
+target_include_directories(${TEST_NAME} PUBLIC ${libint2_INCLUDE_DIRS})
 
 # ... link to this project's library ...
 target_link_libraries(${TEST_NAME} PRIVATE ${LIBRARY_NAME})
