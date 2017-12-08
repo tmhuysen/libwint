cb1696052fc5c787c29e2a71cbde22b300b722dc
Laurent Lemmens <laurent.lemmens@ugent.be>
2017-12-08 16:16:40 +0100

    AO to SO are now wrapper functions that use the base transformation functions.

--- src/transformations.cpp
+++ src/transformations.cpp
@@ -5,7 +5,8 @@
  *      - a matrix h, which contains one-electron integrals in some basis B
  *      - a transformation matrix which represents the transformation of B into B'
  *
- * return the matrix of one-electron integrals h' in the new basis B'.
+ * return the matrix of one-electron integrals h' in the new basis B'
+ *
  *
  *
  *
@@ -13,7 +14,7 @@
  *
  *      B' = B T ,
  *
- *  where the basis vectors are collected as elements of a row vector.
+ *  where the basis vectors are collected as elements of a row vector
  */
 Eigen::MatrixXd libwrp::transform_one_electron_integrals(Eigen::MatrixXd& h, Eigen::MatrixXd& T) {
     return T.adjoint() * h * T;
@@ -24,7 +25,8 @@ Eigen::MatrixXd libwrp::transform_one_electron_integrals(Eigen::MatrixXd& h, Eig
  *      - a rank-four tensor g, which contains the two-electron integrals in some basis B
  *      - a transformation matrix which represents the transformation of B into B'
  *
- * return the tensor g' in the new basis B'.
+ * return the tensor g' in the new basis B'
+ *
  *
  *
  *
@@ -32,7 +34,7 @@ Eigen::MatrixXd libwrp::transform_one_electron_integrals(Eigen::MatrixXd& h, Eig
  *
  *      B' = B T ,
  *
- *  where the basis vectors are collected as elements of a row vector.
+ *  where the basis vectors are collected as elements of a row vector
  */
 Eigen::Tensor<double, 4> libwrp::transform_two_electron_integrals(Eigen::Tensor<double, 4>& g, Eigen::MatrixXd& T) {
 
@@ -45,18 +47,18 @@ Eigen::Tensor<double, 4> libwrp::transform_two_electron_integrals(Eigen::Tensor<
     //      Eigen3 does not accept a way to specify the output axes: instead, it retains the order from left to right of the axes that survive the contraction.
     //      This means that, in order to get the right ordering of the axes, we will have to swap axes
 
-    // g(T U V W)  C^*(V R) -> a(T U R W) but we get a(T U W R)
+    // g(T U V W)  T^*(V R) -> a(T U R W) but we get a(T U W R)
     Eigen::array<Eigen::IndexPair<int>, 1> contraction_pair1 = {Eigen::IndexPair<int>(2, 0)};
     Eigen::array<int, 4> shuffle_1 {0, 1, 3, 2};
 
-    // a(T U R W)  C(W S) -> b(T U R S) and we get b(T U R S), so no shuffle is needed
+    // a(T U R W)  T(W S) -> b(T U R S) and we get b(T U R S), so no shuffle is needed
     Eigen::array<Eigen::IndexPair<int>, 1> contraction_pair2 = {Eigen::IndexPair<int>(3, 0)};
 
-    // C(U Q)  b(T U R S) -> c(T Q R S) but we get c(Q T R S)
+    // T(U Q)  b(T U R S) -> c(T Q R S) but we get c(Q T R S)
     Eigen::array<Eigen::IndexPair<int>, 1> contraction_pair3 = {Eigen::IndexPair<int>(0, 1)};
     Eigen::array<int, 4> shuffle_3 {1, 0, 2, 3};
 
-    // C^*(T P)  c(T Q R S) -> g'(P Q R S) and we get g_SO(p q r s), so no shuffle is needed
+    // T^*(T P)  c(T Q R S) -> g'(P Q R S) and we get g_SO(p q r s), so no shuffle is needed
     Eigen::array<Eigen::IndexPair<int>, 1> contraction_pair4 = {Eigen::IndexPair<int>(0, 0)};
 
 
@@ -77,7 +79,7 @@ Eigen::Tensor<double, 4> libwrp::transform_two_electron_integrals(Eigen::Tensor<
  *  transform and return the matrix in the SO basis
  */
 Eigen::MatrixXd libwrp::transform_AO_to_SO(Eigen::MatrixXd& f_AO, Eigen::MatrixXd& C) {
-    return C.adjoint() * f_AO * C;
+    return transform_one_electron_integrals(f_AO, C);
 }
 
 
@@ -88,7 +90,8 @@ Eigen::MatrixXd libwrp::transform_AO_to_SO(Eigen::MatrixXd& f_AO, Eigen::MatrixX
  *  transform and return the matrix in the AO basis
  */
 Eigen::MatrixXd libwrp::transform_SO_to_AO(Eigen::MatrixXd& f_SO, Eigen::MatrixXd& C){
-    return C.inverse().adjoint() * f_SO * C.inverse();
+    Eigen::MatrixXd C_inverse = C.inverse();
+    return transform_one_electron_integrals(f_SO, C_inverse);
 }
 
 
@@ -99,35 +102,5 @@ Eigen::MatrixXd libwrp::transform_SO_to_AO(Eigen::MatrixXd& f_SO, Eigen::MatrixX
  *  transform and return the two-electron integrals in the SO basis
  */
 Eigen::Tensor<double, 4> libwrp::transform_AO_to_SO(Eigen::Tensor<double, 4>& g_AO, Eigen::MatrixXd& C) {
-
-    // Since we're only getting C as a matrix, we should make the appropriate tensor to perform contractions
-    Eigen::TensorMap<Eigen::Tensor<double, 2>> C_tensor (C.data(), C.rows(), C.cols());
-
-
-    // We will have to do four single contractions, so we specify the contraction indices
-    // Eigen3 does not document its tensor contraction clearly, so see the accepted answer on stackoverflow (https://stackoverflow.com/a/47558349/7930415):
-    //      Eigen3 does not accept a way to specify the output axes: instead, it retains the order from left to right of the axes that survive the contraction.
-    //      This means that, in order to get the right ordering of the axes, we will have to swap axes
-
-    // g_AO(mu nu lambda rho)  C^*(lambda r) -> a(mu nu r rho) but we get a(mu nu rho lambda)
-    Eigen::array<Eigen::IndexPair<int>, 1> contraction_pair1 = {Eigen::IndexPair<int>(2, 0)};
-    Eigen::array<int, 4> shuffle_1 {0, 1, 3, 2};
-
-    // a(mu nu r rho)  C(rho s) -> b(mu nu r s) and we get b(mu nu r s), so no shuffle is needed
-    Eigen::array<Eigen::IndexPair<int>, 1> contraction_pair2 = {Eigen::IndexPair<int>(3, 0)};
-
-    // C(nu q)  b(mu nu r s) -> c(mu q r s) but we get c(q mu r s)
-    Eigen::array<Eigen::IndexPair<int>, 1> contraction_pair3 = {Eigen::IndexPair<int>(0, 1)};
-    Eigen::array<int, 4> shuffle_3 {1, 0, 2, 3};
-
-    // C^*(mu p)  c(mu q r s) -> g_SO(p q r s) and we get g_SO(p q r s), so no shuffle is needed
-    Eigen::array<Eigen::IndexPair<int>, 1> contraction_pair4 = {Eigen::IndexPair<int>(0, 0)};
-
-
-    // Calculate the contractions. We write this as one large contraction to
-    //  1) avoid storing intermediate contractions
-    //  2) let Eigen3 figure out some optimizations
-    Eigen::Tensor<double, 4> g_SO = C_tensor.conjugate().contract(C_tensor.contract(g_AO.contract(C_tensor.conjugate(), contraction_pair1).shuffle(shuffle_1).contract(C_tensor, contraction_pair2), contraction_pair3).shuffle(shuffle_3), contraction_pair4);
-
-    return g_SO;
+    return transform_two_electron_integrals(g_AO, C);
 };
