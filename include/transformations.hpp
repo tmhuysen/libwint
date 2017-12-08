f93b95bc56250e80b7982e7b73289abfd4972455
Laurent Lemmens <laurent.lemmens@ugent.be>
2017-12-08 15:40:15 +0100

    Implemented base one and two-electron transformations.

--- include/transformations.hpp
+++ include/transformations.hpp
@@ -9,13 +9,46 @@ namespace libwrp {
 
 
 /** Given:
+ *      - a matrix h, which contains one-electron integrals in some basis B
+ *      - a transformation matrix which represents the transformation of B into B'
+ *
+ * return the matrix of one-electron integrals h' in the new basis B'.
+ *
+ *
+ *
+ *  Note that the transformation matrix T is used as
+ *
+ *      B' = B T ,
+ *
+ *  where the basis vectors are collected as elements of a row vector.
+ */
+Eigen::MatrixXd transform_one_electron_integrals(Eigen::MatrixXd& h, Eigen::MatrixXd& T);
+
+
+/** Given:
+ *      - a rank-four tensor g, which contains the two-electron integrals in some basis B
+ *      - a transformation matrix which represents the transformation of B into B'
+ *
+ * return the tensor g' in the new basis B'.
+ *
+ *
+ *
+ *  Note that the transformation matrix T is used as
+ *
+ *      B' = B T ,
+ *
+ *  where the basis vectors are collected as elements of a row vector.
+ */
+Eigen::Tensor<double, 4> transform_two_electron_integrals(Eigen::Tensor<double, 4>& g, Eigen::MatrixXd& T);
+
+
+/** Given:
  *      - a matrix representation in an AO basis (f_AO)
  *      - an SO coefficient matrix (every column represents a spatial orbital) (C)
  *
  *  transform and return the matrix in the SO basis
  */
 Eigen::MatrixXd transform_AO_to_SO(Eigen::MatrixXd& f_AO, Eigen::MatrixXd& C);
-
 
 
 /** Given:
--- src/transformations.cpp
+++ src/transformations.cpp
@@ -2,6 +2,75 @@
 
 
 /** Given:
+ *      - a matrix h, which contains one-electron integrals in some basis B
+ *      - a transformation matrix which represents the transformation of B into B'
+ *
+ * return the matrix of one-electron integrals h' in the new basis B'.
+ *
+ *
+ *
+ *  Note that the transformation matrix T is used as
+ *
+ *      B' = B T ,
+ *
+ *  where the basis vectors are collected as elements of a row vector.
+ */
+Eigen::MatrixXd libwrp::transform_one_electron_integrals(Eigen::MatrixXd& h, Eigen::MatrixXd& T) {
+    return T.adjoint() * h * T;
+}
+
+
+/** Given:
+ *      - a rank-four tensor g, which contains the two-electron integrals in some basis B
+ *      - a transformation matrix which represents the transformation of B into B'
+ *
+ * return the tensor g' in the new basis B'.
+ *
+ *
+ *
+ *  Note that the transformation matrix T is used as
+ *
+ *      B' = B T ,
+ *
+ *  where the basis vectors are collected as elements of a row vector.
+ */
+Eigen::Tensor<double, 4> libwrp::transform_two_electron_integrals(Eigen::Tensor<double, 4>& g, Eigen::MatrixXd& T) {
+
+    // Since we're only getting T as a matrix, we should make the appropriate tensor to perform contractions
+    Eigen::TensorMap<Eigen::Tensor<double, 2>> T_tensor (T.data(), T.rows(), T.cols());
+
+
+    // We will have to do four single contractions, so we specify the contraction indices
+    // Eigen3 does not document its tensor contraction clearly, so see the accepted answer on stackoverflow (https://stackoverflow.com/a/47558349/7930415):
+    //      Eigen3 does not accept a way to specify the output axes: instead, it retains the order from left to right of the axes that survive the contraction.
+    //      This means that, in order to get the right ordering of the axes, we will have to swap axes
+
+    // g(T U V W)  C^*(V R) -> a(T U R W) but we get a(T U W R)
+    Eigen::array<Eigen::IndexPair<int>, 1> contraction_pair1 = {Eigen::IndexPair<int>(2, 0)};
+    Eigen::array<int, 4> shuffle_1 {0, 1, 3, 2};
+
+    // a(T U R W)  C(W S) -> b(T U R S) and we get b(T U R S), so no shuffle is needed
+    Eigen::array<Eigen::IndexPair<int>, 1> contraction_pair2 = {Eigen::IndexPair<int>(3, 0)};
+
+    // C(U Q)  b(T U R S) -> c(T Q R S) but we get c(Q T R S)
+    Eigen::array<Eigen::IndexPair<int>, 1> contraction_pair3 = {Eigen::IndexPair<int>(0, 1)};
+    Eigen::array<int, 4> shuffle_3 {1, 0, 2, 3};
+
+    // C^*(T P)  c(T Q R S) -> g'(P Q R S) and we get g_SO(p q r s), so no shuffle is needed
+    Eigen::array<Eigen::IndexPair<int>, 1> contraction_pair4 = {Eigen::IndexPair<int>(0, 0)};
+
+
+    // Calculate the contractions. We write this as one large contraction to
+    //  1) avoid storing intermediate contractions
+    //  2) let Eigen3 figure out some optimizations
+    Eigen::Tensor<double, 4> g_transformed = T_tensor.conjugate().contract(T_tensor.contract(g.contract(T_tensor.conjugate(), contraction_pair1).shuffle(shuffle_1).contract(T_tensor, contraction_pair2), contraction_pair3).shuffle(shuffle_3), contraction_pair4);
+
+    return g_transformed;
+};
+
+
+
+/** Given:
  *      - a matrix representation in an AO basis (f_AO)
  *      - an SO coefficient matrix (every column represents a spatial orbital) (C)
  *
